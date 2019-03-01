254b89cae326380c91b3d29b891d6a290c8d8aa8
Andy Yang <andyyang0830@gmail.com>
2019-03-01 02:26:14 -0500

    add scripts for server and optimize webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  entry: ['./src/renderer/dom.js', '@babel/polyfill'],
+  entry: {
+    app: ['./src/renderer/dom.js'],
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+    filename: '[name].js',
   },
   module: {
     rules: [
-      { test: /\.(js)$/, exclude: /node_modules/, use: 'babel-loader' },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
     ],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
   },
 };
