33ac24b2b578480a7a8c944c785af6a4290f3c40
Andy Yang <andyjungyang@gamil.com>
2019-04-08 13:36:42 +0800

    add passport login andstart configuring dynamo

--- src/server.js
+++ src/server.js
@@ -1,15 +1,34 @@
 import express from 'express';
+import passport from 'passport';
+import {} from 'dotenv/config';
 import config from './config';
+import passportGoogle from './auth/google';
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('view engine', 'ejs');
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.render('index', { answer: 'works' });
 });
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/auth/google', passportGoogle.authenticate('google', {
+  scope: ['profile'],
+}));
+
+app.get('/auth/google/callback',
+  passportGoogle.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/');
+  });
 
 app.listen(config.port, () => {
   console.info(`Running on ${config.port}`);
