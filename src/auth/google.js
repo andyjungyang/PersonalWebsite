c7a56dba43adaae93c51c0ea9678b95d8ba07f0f
Andy Yang <andyjungyang@gamil.com>
2019-04-08 14:23:10 +0800

    actually add auth/dynamyodb

---
+++ src/auth/google.js
@@ -0,0 +1,20 @@
+import passport from 'passport';
+import GoogleStrategy from 'passport-google-oauth20';
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: '/auth/google/callback',
+}, (accessToken, refreshToken, profile, cb) => {
+  cb(null, profile);
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((obj, cb) => {
+  cb(null, obj);
+});
+
+module.exports = passport;
---
+++ src/dynamodb/createUserTable.js
@@ -0,0 +1,29 @@
+import AWS from 'aws-sdk';
+
+AWS.config.update({
+  region: 'us-west-2',
+});
+
+const dynamodb = new AWS.DynamoDB();
+
+const params = {
+  TableName: 'Users',
+  KeySchema: [
+    { AttributeName: 'UserID', KeyType: 'HASH' },
+  ],
+  AttributeDefinitions: [
+    { AttributeName: 'UserID', AttributeType: 'S' },
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 10,
+    WriteCapacityUnits: 10,
+  },
+};
+
+dynamodb.createTable(params, (err, data) => {
+  if (err) {
+    console.error('Unable to create table. Error JSON:', JSON.stringify(err, null, 2));
+  } else {
+    console.log('Created table. Table description JSON:', JSON.stringify(data, null, 2));
+  }
+});
